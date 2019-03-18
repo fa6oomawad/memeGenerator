910da1c083693ae406aa3462979c07aebab95d19
fatima awad <fatimamuk0@gmail.com>
2019-03-17 23:51:00 -0400

    routing added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import one from './one.jpg';
 import './App.css';
-
+import Meme from './Meme.js'
+import {
+  HashRouter,
+  Route,
+  Link
+  
+} from 'react-router-dom';
 class App extends Component {
+
+
   render() {
     return (
+      <HashRouter>
       <div className="App">
        <h1>meme</h1>
        <h1>generator</h1>
        <div className="bigBox">
+      <Link to='/CreateMeme'><div className="smallBox"><img src={one}/></div></Link> 
        <div className="smallBox"></div>
        <div className="smallBox"></div>
        <div className="smallBox"></div>
-       <div className="smallBox"></div>
-       <div className="smallBox"></div>
-       <div className="smallBox"></div>
-       <div className="smallBox"></div>
-
        </div>
+
+
+      <Route path='/CreateMeme' component={Meme} />
       </div>
+      </HashRouter>
     );
   }
 }

646eea3be4729d50f3d7094c3b0e66233cba3fc2
fatima awad <fatimamuk0@gmail.com>
2019-03-17 23:47:15 -0400

    component to handle single meme at a time

---
+++ src/Meme.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import './Meme.css'
+class Meme extends Component {
+render(){
+    return (
+<div className="page">
+ <div className="box">
+
+ </div>        
+</div>
+    );
+
+}
+}
+export default Meme ;

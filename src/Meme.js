f9947d80ccf4031e4c8f18176a9c2e3c8a65d684
fatima awad <fatimamuk0@gmail.com>
2019-03-19 10:01:36 -0400

    a div that allows you to edit the chosen image

--- src/Meme.js
+++ src/Meme.js
@@ -1,13 +1,29 @@
 import React, {Component} from 'react';
-import './Meme.css'
+import App from './App.js';
+import './Meme.css';
+import{
+    HashRouter,
+    Route,
+    Link
+} from 'react-router-dom';
 class Meme extends Component {
+ 
 render(){
     return (
-<div className="page">
- <div className="box">
+        <HashRouter>
 
- </div>        
+<div className="page">
+
+ <div className="box">
+ <Link to="/"><div className="xIcon">X</div></Link>
+ <div className="tinyBox">
+<img src={this.props.imgsrc} alt="meme"/>
+ </div>
+ </div> 
+ <Route path="/" Component={App} />       
 </div>
+</HashRouter>
+
     );
 
 }

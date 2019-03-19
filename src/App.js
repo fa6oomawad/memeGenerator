e2a98aa4bf2ef8cacfd93f447e23b1a9c0e4a53a
fatima awad <fatimamuk0@gmail.com>
2019-03-19 10:07:06 -0400

    passing the clicked img to other component.

--- src/App.js
+++ src/App.js
@@ -9,7 +9,21 @@ import {
   
 } from 'react-router-dom';
 class App extends Component {
+constructor(props){
+super(props);
+this.state={
+  imgsrc:'',
+}
+this.handleImgClick=this.handleImgClick.bind(this);
 
+}
+//to handle the click on meme so we can reach it src to handle it to the other component
+handleImgClick(e){
+console.log(e.target.src);
+this.setState({
+  imgsrc:e.target.src,
+})
+}
 
   render() {
     return (
@@ -18,14 +32,14 @@ class App extends Component {
        <h1>meme</h1>
        <h1>generator</h1>
        <div className="bigBox">
-      <Link to='/CreateMeme'><div className="smallBox"><img src={one}/></div></Link> 
+      <Link to='/CreateMeme'><div className="smallBox" onClick={this.handleImgClick}><img src={one} alt='meme'/></div></Link> 
        <div className="smallBox"></div>
        <div className="smallBox"></div>
        <div className="smallBox"></div>
        </div>
 
 
-      <Route path='/CreateMeme' component={Meme} />
+      <Route path='/CreateMeme' render={(props)=><Meme {...props} imgsrc={this.state.imgsrc}/>} />
       </div>
       </HashRouter>
     );

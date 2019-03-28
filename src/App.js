e18a4846a52fc61a6fe7337ccea568523fbe6a07
fatima awad <fatimamuk0@gmail.com>
2019-03-28 19:13:57 -0400

    displaying memes from API

--- src/App.js
+++ src/App.js
@@ -13,10 +13,33 @@ constructor(props){
 super(props);
 this.state={
   img:'',
+  data:[]
 }
 this.handleImgClick=this.handleImgClick.bind(this);
-
+this.fetchingMemes=this.fetchingMemes.bind(this);
 }
+
+componentDidMount(){
+  this.fetchingMemes();
+}
+fetchingMemes(){
+  const encodedURI=encodeURI(`http://version1.api.memegenerator.net//Generators_Select_ByPopular?pageIndex=0&pageSize=12&apiKey=7ca1ef06-a4d3-46ca-8d9c-78533b6b5e78`)
+  return fetch(encodedURI)
+  .then((data)=>data.json())
+  .then((repos)=>{
+    this.setState({data:repos.result})
+  }
+)
+.catch((error)=>{
+  console.warn(error)
+  return null 
+});
+}
+componentDidUpdate(){
+  console.log('update',this.state.data)
+}
+
+
 //to handle the click on meme so we can reach it src to handle it to the other component
 handleImgClick(e){
 console.log(e.target);
@@ -29,13 +52,20 @@ this.setState({
     return (
       <HashRouter>
       <div className="App">
-       <h1>meme</h1>
-       <h1>generator</h1>
+      <div className='titleBox'>
+      <h1 className="title">MEME <br/> GENERATOR</h1>
+      </div>
+     
        <div className="bigBox">
-      <Link to='/CreateMeme'><div className="smallBox" onClick={this.handleImgClick}><img src={one} alt='meme'/></div></Link> 
-       <div className="smallBox"></div>
-       <div className="smallBox"></div>
-       <div className="smallBox"></div>
+      
+       {this.state.data.map((meme)=>{
+         return ( 
+         <Link to='/CreateMeme'>
+         <div className="smallBox" onClick={this.handleImgClick}><img src={meme.imageUrl} alt='meme'/>
+         </div>
+         </Link> 
+         )
+       })}
        </div>
 
 

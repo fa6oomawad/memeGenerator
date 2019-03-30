5e0d720af92887775f35877d9cb5744e843373b6
fatima awad <fatimamuk0@gmail.com>
2019-03-30 15:30:11 -0400

    Updates

---
+++ precache-manifest.1a95443317e7353a51ee57d03657f5de.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "35c4ac284a85bf2d60999e13e3b708eb",
+    "url": "/memeGenerator/static/media/sketch.35c4ac28.ttf"
+  },
+  {
+    "revision": "f242473074cfb69336b55fb1d5c3fc37",
+    "url": "/memeGenerator/static/media/one.f2424730.jpg"
+  },
+  {
+    "revision": "5131287b336b685cdfe4",
+    "url": "/memeGenerator/static/js/runtime~main.7d2c4351.js"
+  },
+  {
+    "revision": "9386ca70c860fcb1ff8d",
+    "url": "/memeGenerator/static/js/main.6c8b5c9b.chunk.js"
+  },
+  {
+    "revision": "49c015c8521c8d69aca3",
+    "url": "/memeGenerator/static/js/2.7809389b.chunk.js"
+  },
+  {
+    "revision": "9386ca70c860fcb1ff8d",
+    "url": "/memeGenerator/static/css/main.1176ccb1.chunk.css"
+  },
+  {
+    "revision": "3b68431a8226cd5f8fb948f8ee2575df",
+    "url": "/memeGenerator/index.html"
+  }
+];

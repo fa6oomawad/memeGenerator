1945b6e8b26b10065af1a2f172a444bf3f9540ec
fatima awad <fatimamuk0@gmail.com>
2019-04-20 23:27:32 -0400

    remove some route

--- src/App.js
+++ src/App.js
@@ -81,7 +81,7 @@ handleMouseOut(e){
        })}
        </div>
 
-       <Route  path='/' render={(props)=><App {...props} />} />
+
 
       <Route path='/CreateMeme' render={(props)=><Meme {...props} img={this.state.img}/>} />
       </div>
